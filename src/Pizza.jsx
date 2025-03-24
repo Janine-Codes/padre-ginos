946444e294492718d76e8e9297ba65935d700747
Janine-Codes <janinekrse@gmail.com>
2025-03-24 14:31:25 +0100

    html markup

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,11 +1,15 @@
+let counter = 0;
 const Pizza = (props) => {
-    return (
-      <div className="pizza">
-        <h1>{props.name}</h1>
-        <p>{props.description}</p>
-        <img src={props.image} alt={props.name} />
-      </div>
-    );
-  };
-  
-  export default Pizza;
+  counter = Date.now();
+  return (
+    <div className="pizza">
+      <h1>
+        {props.name} {counter}
+      </h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
+
+export default Pizza;
